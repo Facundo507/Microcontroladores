b7a53fb03edff0ce19cb3c5172d3374d0150c50d
IgnacioDSA <ignacio.dasilveira@estudiantes.utec.edu.uy>
2024-11-04 00:28:15 -0300

    Add files via upload

---
+++ Laboratorio 3/Plotter/Plotter/Plotter/main.c
@@ -0,0 +1,85 @@
+/*
+ * main.c
+ *
+ * Created: 11/3/2024 9:46:08 PM
+ *  Author: dasil
+ */ 
+#define F_CPU 1000000UL
+#include <xc.h>
+#include <avr/io.h>
+#include <avr/delay.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define RelojX PB3
+#define DirX PB4
+#define EnableX PB5
+
+#define Solenoide PC0
+
+#define RelojY PC3
+#define DirY PC4
+#define EnableY PC5
+
+#define Limite_Y_A PD2
+#define Limite_Y_D PD3
+
+#define LED PD5
+
+
+void UART_init(unsigned int ubrr) {
+	// Configurar el valor de UBRR (Baud Rate Register)
+	UBRR0H = (unsigned char)(ubrr >> 8);
+	UBRR0L = (unsigned char)ubrr;
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void UART_sendChar(char data) {
+	while (!(UCSR0A & (1 << UDRE0))){}
+	UDR0 = data;
+}
+
+char UART_receiveChar(void) {
+	while (!(UCSR0A & (1 << RXC0))){}
+	return UDR0;
+}
+
+void UART_sendString(const char *str) {
+	while (*str) {
+		UART_sendChar(*str++);
+	}
+}
+
+void int0_init(){
+	DDRD &= ~(1 << PD2); //PD2 como entrada (INT0)
+	//PORTD |= (1 << PD2); //Resistencia pull-up
+	
+	// Condiguracion interrupcion INT0 flanco de subida.
+	
+	EICRA |= (1 << ISC01);
+	EICRA |= (1 << ISC00);
+	
+	EIMSK |= (1 << INT0);
+	sei();
+}
+
+ISR(INT0_vect) {
+
+
+}
+
+int main(void) {
+	UART_init(103);
+	DDRB |= (1 << RelojX) | (1 << DirX) | (1 << EnableX);
+	DDRC |= (1 << Solenoide) | (1 << RelojY) | (1 << DirY) | (1 << EnableY);
+	DDRD |= (1 << Limite_Y_A) | (1 << Limite_Y_D) | (1 << LED);
+	
+	
+	int0_init();             // Inicializa la interrupción INT0
+	while (1) {
+		UART_sendString("Bienvenido al programa del plotter en Atmega328P\r\n");
+		UART_sendString("\r\n");
+		
+	}
+}
